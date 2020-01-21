64a882b17b486190c033f6342414facc9fa17bac
ethercat <stephenyu113@outlook.com>
2020-01-21 11:33:07 -0500

    first commit

---
+++ scripts/move_group_interface.py
@@ -0,0 +1,220 @@
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+
+def all_close(goal, actual, tolerance):
+    """
+    Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
+    @param: goal       A list of floats, a Pose or a PoseStamped
+    @param: actual     A list of floats, a Pose or a PoseStamped
+    @param: tolerance  A float
+    @returns: bool
+    """
+    all_equal = True
+    if type(goal) is list:
+        for index in range(len(goal)):
+            if abs(actual[index] - goal[index]) > tolerance:
+                return False
+
+    elif type(goal) is geometry_msgs.msg.PoseStamped:
+        return all_close(goal.pose, actual.pose, tolerance)
+
+    elif type(goal) is geometry_msgs.msg.Pose:
+        return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+
+    return True
+
+
+class MoveGroupInterface(object):
+
+    def __init__(self):
+        super(MoveGroupInterface, self).__init__()
+
+        ## SETUP
+        # First initialize `moveit_commander`_ and a `rospy`_ node:
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('move_group_interface', anonymous=True)
+
+        # Instantiate a `RobotCommander`_ object. This object is the outer-level interface to the robot
+        robot = moveit_commander.RobotCommander
+
+        # Instantiate a `PlanningSceneInterface`_ object.  This object is an interface
+        # to the world surrounding the robot:
+        scene = moveit_commander.PlanningSceneInterface()
+
+        # Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+        # to one group of joints.  In this case the group is the joints in the UR5e
+        # arm so we set ``group_name = ur5e``. If you are using a different robot,
+        # you should change this value to the name of your robot arm planning group.
+        # This interface can be used to plan and execute motions on the Panda:
+        group_name = "manipulator"
+        group = moveit_commander.MoveGroupCommander(group_name)
+
+        # We create a `DisplayTrajectory`_ publisher which is used later to publish
+        # trajectories for RViz to visualize:
+        display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                       moveit_msgs.msg.DisplayTrajectory,
+                                                       queue_size=20)
+
+        ## BASIC_INFO
+        # Getting basic information
+        # We can get the name of the reference frame for this robot:
+        planning_frame = group.get_planning_frame()
+        print "============ Reference frame: %s" % planning_frame
+
+        # We can also print the name of the end-effector link for this group:
+        eef_link = group.get_end_effector_link()
+        print "============ End effector: %s" % eef_link
+
+        # We can get a list of all the groups in the robot:
+        # group_names = robot.get_group_names()
+        # print "============ Robot Groups:", robot.get_group_names()
+
+        # Sometimes for debugging it is useful to print the entire state of the
+        # robot:
+        # print "============ Printing robot state"
+        # print robot.get_current_state()
+        print ""
+
+        # Misc variables
+        self.box_name = ''
+        self.robot = robot
+        self.scene = scene
+        self.group = group
+        self.display_trajectory_publisher = display_trajectory_publisher
+        self.planning_frame = planning_frame
+        self.eef_link = eef_link
+        # self.group_names = group_names
+
+    def go_to_joint_state(self):
+        # We can get the joint values from the group and adjust some of the values:
+        joint_goal = self.group.get_current_joint_values()
+        joint_goal[0] = 0
+        joint_goal[1] = -pi / 4
+        joint_goal[2] = 0
+        joint_goal[3] = -pi / 2
+        joint_goal[4] = 0
+        joint_goal[5] = pi / 3
+        # joint_goal[6] = 0
+
+        # The go command can be called with joint values, poses, or without any
+        # parameters if you have already set the pose or joint target for the group
+        self.group.go(joint_goal, wait=True)
+
+        # Calling ``stop()`` ensures that there is no residual movement
+        self.group.stop()
+
+        current_joints = self.group.get_current_joint_values()
+        return all_close(joint_goal, current_joints, 0.01)
+
+    def go_to_pose_goal(self):
+        # We can plan a motion for this group to a desired pose for the
+        # end-effector:
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.orientation.w = 1.0
+        pose_goal.position.x = 0.4
+        pose_goal.position.y = 0.1
+        pose_goal.position.z = 0.4
+        self.group.set_pose_target(pose_goal)
+
+        # Now, we call the planner to compute the plan and execute it.
+        plan = self.group.go(wait=True)
+        # Calling `stop()` ensures that there is no residual movement
+        self.group.stop()
+        # It is always good to clear your targets after planning with poses.
+        # Note: there is no equivalent function for clear_joint_value_targets()
+        self.group.clear_pose_targets()
+
+        current_pose = self.group.get_current_pose().pose
+        return all_close(pose_goal, current_pose, 0.01)
+
+    def plan_cartesian_path(self, scale=1):
+        # You can plan a Cartesian path directly by specifying a list of waypoints
+        # for the end-effector to go through:
+        waypoints = []
+
+        wpose = self.group.get_current_pose().pose
+        wpose.position.z -= scale * 0.1  # First move up (z)
+        wpose.position.y += scale * 0.2  # and sideways (y)
+        waypoints.append(copy.deepcopy(wpose))
+
+        wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
+        waypoints.append((copy.deepcopy(wpose)))
+
+        wpose.position.y -= scale * 0.1  # Third move sideways (y)
+        waypoints.append(copy.deepcopy(wpose))
+
+        # We want the Cartesian path to be interpolated at a resolution of 1 cm
+        # which is why we will specify 0.01 as the eef_step in Cartesian
+        # translation.  We will disable the jump threshold by setting it to 0.0 disabling:
+        (plan, fraction) = self.group.compute_cartesian_path(
+                                                waypoints=waypoints,    # waypoints to follow
+                                                eef_step=0.01,          # eef_step
+                                                jump_threshold=0.0)     # jump_threshold
+
+        # Note: We are just planning, not asking move_group to actually move the robot yet:
+        return plan, fraction
+
+    def display_trajectory(self, plan):
+        # A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
+        # We populate the trajectory_start with our current robot state to copy over
+        # any AttachedCollisionObjects and add our plan to the trajectory.
+        display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+        display_trajectory.trajectory_start = self.robot.get_current_state()
+        display_trajectory.trajectory.append(plan)
+        # Publish
+        self.display_trajectory_publisher.publish(display_trajectory)
+
+    def execute_plan(self, plan):
+        # Use execute if you would like the robot to follow
+        # the plan that has already been computed:
+        self.group.execute(plan)
+
+        # **Note:** The robot's current joint state must be within some tolerance of the
+        # first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
+
+
+def main():
+    try:
+        print "============ Press `Enter` to begin the tutorial by " \
+              "setting up the moveit_commander (press ctrl-d to exit) ..."
+        raw_input()
+        interface = MoveGroupInterface()
+
+        print "============ Press `Enter` to execute a movement using a joint state goal ..."
+        raw_input()
+        interface.go_to_joint_state()
+
+        print "============ Press `Enter` to execute a movement using a pose goal ..."
+        raw_input()
+        interface.go_to_pose_goal()
+
+        print "============ Press `Enter` to plan and display a Cartesian path ..."
+        raw_input()
+        cartesian_path, fraction = interface.plan_cartesian_path()
+
+        print "============ Press `Enter` to plan and display a Cartesian path ..."
+        raw_input()
+        interface.display_trajectory(cartesian_path)
+
+        print "============ Press `Enter` to execute a saved path ..."
+        raw_input()
+        interface.execute_plan(cartesian_path)
+
+        print "============ Demo complete!"
+
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/ur5e_controller.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+
+"""Custom velocity controller for UR5e"""
+
+from time import time
+
+import numpy as np
+import rospy
+import roslib; roslib.load_manifest('ur_driver')
+import sensor_msgs.msg as sensor_msgs
+from std_msgs.msg import String
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+import geometry_msgs.msg as geometry_msgs
+
+# ur_kinematics (IK)
+#import ur_kin_py
+#from ur_kinematics import ur_kin_py
+
+class Arm:
+
+    def __init__(self):
+        '''Create arm object'''
+
+        # basic assignments
+        self.endEffector =  "ee_link"
+
+        # joint values and joint limits
+        self.jointValues = [0] * 6
+        self.joint_velocities = [0] * 6
+        self.joint_names = [''] * 6
+        #TODO set joint limmits from movit
+        self.joint_limits = np.zeros((2,6))
+        self.joint_limits[0,:] = -np.pi
+        self.joint_limits[1,:] = np.pi
+        # self.robot.SetDOFLimits(self.joint_limits[0], self.joint_limits[1])
+
+        # Ordering of joints.
+        self.joint_names_speedj = ['shoulder_pan_joint', 'shoulder_lift_joint', \
+           'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+
+        # subscribers
+        self.joints_sub = rospy.Subscriber("/joint_states", sensor_msgs.JointState, self.jointsCallback)
+        self.joints_sub = rospy.Subscriber("/wrench", geometry_msgs.WrenchStamped, self.forceCallback)
+        self.useURScript = True
+        self.pub = rospy.Publisher('/ur_hardware_interface/script_command', String, queue_size=10)
+
+        #no collision checking
+        self.obstacleCloud = None
+
+        # register callback for CTRL+C
+        rospy.on_shutdown(self.shutdownCallback)
+
+    def calcFK(self, config, endEffectorName=None):
+        '''Calculate the Cartesian poses for a given collection of joint positions.'''
+
+        if endEffectorName is None:
+          endEffectorName = self.endEffector
+
+        self.robot.SetDOFValues(config, self.manip.GetArmIndices())
+        return self.robot.GetLink(endEffectorName).GetTransform()
+
+#    def calcIKForT(self, T, ignoreHandCollisions=False, forceOpenrave=False):
+#        '''Uses UR IK solvers to find solutions.'''
+#
+#        # Call the IK solver in universal_robot/ur_kinematics
+#        sols = ur_kin_py.inverse(np.ascontiguousarray(T), 0.0)
+#
+#        # Check for joint limits and collisions
+#        valid = []
+#        for i, sol in enumerate(sols):
+#            if np.isnan(sol[0]): continue #ignore null solutions
+#
+#            # wrap joint positions such that they are within the limits.
+#            larger2np.pi = np.logical_and(sol > self.joint_limits[1, :], sol - 2 * np.pi > self.joint_limits[0, :])
+#            sol[np.where(larger2np.pi)] -= 2 * np.pi
+#            smaller2np.pi = np.logical_and(sol < self.joint_limits[0, :], sol + 2 * np.pi < self.joint_limits[1, :])
+#            sol[np.where(smaller2np.pi)] += 2 * np.pi
+#
+#            # Check if the config is within the joint limits.
+#            isInLimits = self.isInJointLimits(sol)
+#
+#            if isInLimits:
+#                valid.append(sol)
+#
+#        # Return results, if any.
+#        return valid
+
+    def findClosestIK(self, solutions):
+        '''Find the joint positions closest to the current joint positions.'''
+
+        return self.findClosestIKToGiven(solutions, self.jointValues)
+
+    def findClosestIKToGiven(self, solutions, jointsStart):
+        '''Find the joint positions closest to the given joint positions.'''
+
+        closestSolution = None
+        minDist = float('inf')
+        for solution in solutions:
+            diff = jointsStart - solution
+            dist = np.dot(diff, diff)
+            if dist < minDist:
+                minDist = dist
+                closestSolution = solution
+        return closestSolution
+
+    def publishVelocities(self, velocities_dict):
+        '''Sends velocity message to arm '''
+        a = 0.1
+        t = 0.1
+        vels = [velocities_dict['shoulder_pan_joint'],
+                velocities_dict['shoulder_lift_joint'],
+                velocities_dict['elbow_joint'],
+                velocities_dict['wrist_1_joint'],
+                velocities_dict['wrist_2_joint'],
+                velocities_dict['wrist_3_joint']]
+        self.pub.publish('speedj(' + str(vels) + ', ' + str(a) \
+                             + ', ' + str(t) + ')')
+
+    def followTrajectory(self, traj, gain, maxDistToTarget, gamma=0.97, breakGamma=0.75, maxErrorMag=0.80, isBraking=True):
+        '''Simple leaky integrator with error scaling and breaking.'''
+
+        first_half_len = int(len(traj)/2)
+        gains = np.array([gain]*len(traj))
+        gains[first_half_len : ] = np.linspace(gain, 0.8, len(traj) - first_half_len)
+        maxDistsToTarget = [maxDistToTarget]*(len(traj)-1) + [maxDistToTarget - 0.002]
+
+        leakySum = 0.0
+        command = np.zeros(6)
+
+        print("Moving arm...")
+        avg_vels = []
+        max_vels = []
+        for i, target in enumerate(traj):
+            error = target - self.jointValues
+
+            while np.max(np.abs(error)) > maxDistsToTarget[i]:
+                t0 = time()
+
+                # scale to maximum error
+                errorMag = np.linalg.norm(error)
+                if errorMag > maxErrorMag:
+                    scale = maxErrorMag / errorMag
+                    error = scale * error
+
+                # integrate error
+                leakySum = gamma*leakySum + (1.0-gamma)*error
+                command = gains[i]*leakySum
+
+                self.publishVelocities(self.createJointVelocitiesDict(command))
+
+                avg_vels.append(np.mean(command))
+                max_vels.append(np.max(np.abs(command)))
+
+                # sleepTime = 0.008 - (time() - t0)
+                sleepTime = 0.008
+                # print('  sleepTime:', sleepTime)
+                rospy.sleep(sleepTime)
+                # rospy.sleep(0.008)
+
+                # compute error
+                error = target - self.jointValues
+
+            print('[followTrajectory] Reached viapoint #', i, 'with error:', np.max(np.abs(error)), np.abs(error))
+
+        with open('/home/ur5e/.ros/avg_vels.txt', 'w') as f:
+            for v in avg_vels:
+                f.write('%.8f\n' % v)
+
+        with open('/home/ur5e/.ros/max_vels.txt', 'w') as f:
+            for v in max_vels:
+                f.write('%.8f\n' % v)
+
+        # Reduce velocity to 0.
+        if isBraking:
+            print("Braking...")
+            self.brake(command, breakGamma)
+
+        # set speed to 0.0
+        self.publishVelocities(self.createJointVelocitiesDict(np.zeros(6)))
+
+    def brakeSmooth(self, command, thresh=1e-6):
+        diff = np.sum(command)
+
+        while np.max(np.abs(command)) > thresh:
+            t0 = time()
+            command = gamma*command
+            self.publishVelocities(self.createJointVelocitiesDict(command))
+            sleepTime = 0.008
+            rospy.sleep(sleepTime)
+
+    def brake(self, command, gamma, thresh=1e-6):
+        while max(np.abs(command)) > thresh:
+            t0 = time()
+            command = gamma*command
+            self.publishVelocities(self.createJointVelocitiesDict(command))
+            sleepTime = 0.008
+            rospy.sleep(sleepTime)
+
+    def forceCallback(self, msg):
+        '''Callback function for the joint_states ROS tonp.pic.'''
+        self.force_mag = msg.wrench.force.x ** 2 + msg.wrench.force.y ** 2 + msg.wrench.force.z ** 2
+
+    def isInJointLimits(self, joint_positions):
+        '''Returns true only if the given arm position is within the joint limits.'''
+        return (joint_positions >= self.joint_limits[0, :]).all() and (joint_positions <= self.joint_limits[1, :]).all()
+
+    def jointsCallback(self, msg):
+        '''Callback function for the joint_states ROS tonp.pic.'''
+        positions_dict = jointStateToDict(msg)
+        self.joint_positions_dict = positions_dict
+        self.jointValues = np.array([positions_dict[i] for i in self.joint_names_speedj])
+
+    def shutdownCallback(self):
+        '''Gradually reduces the joint velocities to zero when the program is trying to shut down.'''
+
+        print("Received shutdown signal ...")
+
+        v = np.asarray(self.joint_velocities)
+        print 'arm velocities:', v
+
+        if np.linalg.norm(v) >= 0.01:
+            print("Arm is moving. Slowing down to avoid hard braking.")
+            max_iterations = 100
+            vel = v
+            step = v / max_iterations
+
+            for i in range(0, max_iterations):
+                vel -= step
+                self.publishVelocities(self.createJointVelocitiesDict(vel))
+                rospy.sleep(0.01)
+
+            # Reduce speed to zero.
+            self.publishVelocities(self.createJointVelocitiesDict(np.zeros(6)))
+            rospy.sleep(0.01)
+
+        print("Exiting ...")
+        exit(0)
+
+    def createJointVelocitiesDict(self, velocities):
+        dict_out = {}
+        for i in range(len(velocities)):
+            dict_out[self.joint_names_speedj[i]] = velocities[i]
+        return dict_out
+
+def jointStateToDict(msg):
+    '''Convert JointState message to dictionary.'''
+    dict_out = {}
+    for i in range(len(msg.position)):
+        dict_out[msg.name[i]] = msg.position[i]
+
+    return dict_out
